ca511bed87c821f3611ddaf863212072e3db3ac6
Adaseja <lisicka.adrianna@gmail.com>
2023-05-12 21:53:01 +0200

    implement bubble sort algorithm in JavaScript

---
+++ 8-sortowanie-babelkowanie/main.js
@@ -0,0 +1,8 @@
+function sortNum (a, b) {
+    return a - b;
+  }
+  
+  const arr =  [12, 67, 34, 23.01, 24, 2, 56, 8, 0x10, 23.02, 78, 34, 1e2, 45, 67, 98, 1];
+  arr.sort(sortNum);
+  
+  console.log(arr);
