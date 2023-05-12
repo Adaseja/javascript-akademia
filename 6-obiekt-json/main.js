81244fbfb2294962f3fe43aae0d8ebc4efac73c3
Adaseja <lisicka.adrianna@gmail.com>
2023-05-12 19:25:43 +0200

    utilized forEach loop to iterate over each employee in the array

---
+++ 6-obiekt-json/main.js
@@ -0,0 +1,22 @@
+let jsonPracownicy = {
+    "pracownicy": [
+        {"firstName": "Krystian", "lastName": "Dziopa"}, 
+        {"firstName": "Anna", "lastName": "Szapiel"},
+        {"firstName": "Piotr", "lastName": "Żmuda"}
+    ]
+};
+
+for (let pracownik of jsonPracownicy.pracownicy) {
+    console.log(pracownik.lastName + " "+  pracownik.firstName)
+    console.log(`pracownik: ${pracownik.lastName}   ${pracownik.firstName}`)
+}
+
+
+/* bezimienna funkcja strzalkowa */
+
+jsonPracownicy.pracownicy.forEach((pracownik, index) => {
+    console.log(pracownik.lastName + " "+  pracownik.firstName)
+    console.log(`pracownik: ${pracownik.lastName}   ${pracownik.firstName} posiada index ${index}`)
+} )
+
+console.log(JSON.parse(jsonPracownicy));
