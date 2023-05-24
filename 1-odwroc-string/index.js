a90473ccf9172ab690ad541a81c02eabbd19b90d
adaseja <106311865+Adaseja@users.noreply.github.com>
2023-05-24 09:36:32 +0200

    Delete 1-odwroc-string directory

--- 1-odwroc-string/index.js
+++
@@ -1,9 +0,0 @@
-function reverseString(str) {
-    return str.split('').reverse().join('');
-  }
-  
-  let name = 'Akademia108';
-  let reversedName = reverseString(name);
-  
-  console.log(reversedName);
-  
