4db0777eb37737e1b411408acdd25012b1c5712c
Adaseja <lisicka.adrianna@gmail.com>
2023-05-29 10:17:58 +0200

    working on function

---
+++ jquery-javascript/3-suma-wyplat/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#count-sum").click(function() {
+      
+      $(".salary").each(function() {
+        $(this).salary('.salary', sum);
+        sum = salary
+     
+    });
+    $("#sum").text(sum + " PLN");
+  });
+});
